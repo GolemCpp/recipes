6262853ee1bd65bc6bd9be0b831660d521d8f728
Barnabé BALP <contact@barnabebalp.fr>
2020-08-24 20:52:33 +0000

    Fix build of boost for CI

--- boost@com.github.boostorg/golemfile.py
+++ boost@com.github.boostorg/golemfile.py
@@ -6,6 +6,7 @@ import sys
 import subprocess
 import distutils
 import multiprocessing
+import shutil
 
 
 def configure(project):
@@ -22,6 +23,9 @@ def configure(project):
         'boost_unit_test_framework', 'boost_wave', 'boost_wserialization'
     ]
 
+    def target_decorator(target_name, config, context):
+        return target_name
+
     def artifacts_generator(decorated_target, config, context):
         artifacts = []
         for suffix in context.artifact_suffix(config):
@@ -36,6 +40,7 @@ def configure(project):
     project.library(name='boost',
                     targets=targets,
                     scripts=[script],
+                    target_decorators=[target_decorator],
                     artifacts_generators=[artifacts_generator])
 
     project.export(name='boost',
@@ -54,16 +59,51 @@ def configure(project):
     # 'boost_test_exec_monitor'
 
 
+def make_install_path(ctx):
+    out_path = ctx.make_out_path()
+    install_folder = os.path.basename(out_path) + "-install"
+    prefix_dir = os.path.join(os.path.dirname(out_path), install_folder)
+    if not os.path.exists(prefix_dir):
+        os.makedirs(prefix_dir)
+    return prefix_dir
+
+
 def script(ctx):
 
     boost_dir = ctx.get_project_dir()
+    target_dir = ctx.context.out_dir
 
-    bootstrap_bin = ctx.msvc_vcvars_cmd() + 'bootstrap.bat' if ctx.is_windows(
-    ) else './bootstrap.sh'
+    bootstrap_opt_libs = []
+    bootstrap_opt_libs.append('--without-libraries=python')
 
-    ret = subprocess.call(bootstrap_bin, cwd=boost_dir, shell=True)
+    msvc_cmd = ''
+    if ctx.is_windows():
+        msvc_cmd = ctx.msvc_vcvars_cmd()
+
+    bootstrap_bin = 'bootstrap.bat' if ctx.is_windows() else './bootstrap.sh'
+
+    cmd = msvc_cmd + ' '.join([bootstrap_bin] + bootstrap_opt_libs)
+
+    if ctx.is_windows():
+        cmd = 'cmd /V /C "{}"'.format(cmd)
+
+    print("Run command: {}".format(cmd))
+    ret = subprocess.call(cmd, cwd=boost_dir, shell=True)
     if ret:
         raise RuntimeError("ERROR: " + bootstrap_bin)
+
+    boost_build_dir = os.path.join(target_dir, 'boost-build')
+    boost_stage_dir = os.path.join(target_dir, 'boost-stage')
+
+    if os.path.exists(boost_build_dir):
+        shutil.rmtree(boost_build_dir)
+
+    if os.path.exists(boost_stage_dir):
+        shutil.rmtree(boost_stage_dir)
+
+    opt_dirs = [
+        '--build-dir=' + boost_build_dir, '--stagedir=' + boost_stage_dir
+    ]
 
     opt_variant = 'variant='
     if ctx.is_debug():
@@ -87,26 +127,37 @@ def script(ctx):
     if not ctx.is_windows():
         opt_headers = ['headers']
 
+    if ctx.is_windows():
+        msvc_cmd += 'set "VS150COMNTOOLS=!VS160COMNTOOLS!" && '  # workaround
+
+    prefix_dir = make_install_path(ctx)
+
     b2_bin = 'b2' if ctx.is_windows() else './b2'
-    ret = subprocess.call([
-        b2_bin, '-j' + str(multiprocessing.cpu_count()), '--layout=system',
+    cmd = msvc_cmd + ' '.join([
+        b2_bin, '-a', '-j' +
+        str(multiprocessing.cpu_count()), '--layout=system',
         'define=BOOST_ASIO_DISABLE_THREAD_KEYWORD_EXTENSION', opt_variant,
         opt_arch, opt_runtime, opt_link
-    ] + opt_libs + opt_headers,
-                          cwd=boost_dir,
-                          shell=True)
+    ] + opt_dirs + opt_libs + ['install', '--prefix=' + prefix_dir])
+
+    if ctx.is_windows():
+        cmd = 'cmd /V /C "{}"'.format(cmd)
+
+    print("Run command: {}".format(cmd))
+    ret = subprocess.call(cmd, cwd=boost_dir, shell=True)
     if ret:
         raise RuntimeError("ERROR: b2")
 
     out_path = ctx.make_out_path()
-    distutils.dir_util.copy_tree(os.path.join(boost_dir, 'stage', 'lib'),
+    distutils.dir_util.copy_tree(os.path.join(prefix_dir, 'lib'),
                                  out_path,
                                  preserve_symlinks=1)
 
-    if ctx.is_windows():
-        ret = subprocess.call([b2_bin, 'headers'], cwd=boost_dir, shell=True)
-        if ret:
-            raise RuntimeError("ERROR: b2 headers")
-
-    distutils.dir_util.copy_tree(os.path.join(boost_dir, 'boost'),
+    distutils.dir_util.copy_tree(os.path.join(prefix_dir, 'include', 'boost'),
                                  os.path.join(boost_dir, 'include', 'boost'))
+
+    if os.path.exists(boost_build_dir):
+        shutil.rmtree(boost_build_dir)
+
+    if os.path.exists(boost_stage_dir):
+        shutil.rmtree(boost_stage_dir)
